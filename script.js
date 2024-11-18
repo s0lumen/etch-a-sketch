6b7bc0c77c95c6c27894da872f2dc04f05036b2e
Steve <173348548+s0lumen@users.noreply.github.com>
2024-11-18 11:49:22 +0800

    changed approach

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const slider = document.querySelector("#slider");
+const sketchGrid = document.querySelector(".sketchContainer");
+let slider = document.querySelector("#slider")
 
-//sizeSlider.onmousemove = (e) => console.log(e.target.value);
 
-slider.addEventListener("mousemove", (e) => {
-    console.log(e.target.value);
-});
-slider.addEventListener("change", (e) => {
-    console.log(e.target.value);
-});
+slider.onmousemove = (e) => console.log(e.target.value);
+slider.onchange = (e) => console.log(e.target.value);
 
+function sketchBox(size) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+
+    return box;
+}
+
+//function sketchGrid(dimension) {
+//    for ()
+//}
