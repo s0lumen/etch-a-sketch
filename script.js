e84cd2e6a689bc6f3e1af0487b4c7e7fdd611488
Steve <173348548+s0lumen@users.noreply.github.com>
2024-11-22 10:16:32 +0800

    added buttons, can now makes grids

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
-const sketchContainer = document.querySelector(".sketchContainer");
-let slider = document.querySelector("#slider")
+const GRID_DIM = 500;
+
+const sketchContainer = document.querySelector('.sketchContainer');
+let slider = document.querySelector('#slider')
 
 
 slider.onmousemove = (e) => console.log(e.target.value);
-slider.onchange = (e) => console.log(e.target.value);
+// slider.onchange = (e) => newValue(e.target.value);
+// slider.addEventListener("change", (e) => ) 
 
-function sketchBox(size) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
 
-    return box;
-}
+function createTile () {
+    let dimension = slider.value;
 
-function sketchGrid(dimension) {
-    for (let i = 0; i < dimension; i++) {
-        sketchContainer.appendChild(sketchBox(sketchContainer.clientWidth / dimension));
-        
+    for (i = 0; i < dimension*dimension; i++){
+        const tiles = document.createElement('div');
+        tiles.classList.add('tiles');
+        tiles.style.width = `${GRID_DIM / dimension}px`;
+        tiles.style.height = `${GRID_DIM / dimension}px`;
+
+        sketchContainer.appendChild(tiles);
     }
-}
+}  
 
-sketchGrid(16);
-
-//function sketchGrid(dimension) {
-//    for ()
-//}
+createTile();
