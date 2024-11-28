75597fc035c8ba33f4ea749d77f1fe98a66df18e
Steve <173348548+s0lumen@users.noreply.github.com>
2024-11-28 09:02:05 +0800

    added first parts of mode functions

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const GRID_DIM = 500;
+const STARTMODE = 'color'
+
+let sketchColor = STARTMODE
 
 const sketchContainer = document.querySelector('.sketchContainer');
-const colorPicker = document.querySelector('#colorPicker')
-let slider = document.querySelector('#slider')
-
+const colorPicker = document.querySelector('#colorPicker');
+let slider = document.querySelector('#slider');
+let btnColor = document.querySelector('#Color');
+let btnEraser = document.querySelector('#Eraser');
+let btnRandom = document.querySelector('#Random');
 
 slider.onmousemove = (e) => console.log(e.target.value);
 slider.onchange = (e) => console.log(e.target.value);
 colorPicker.oninput =  (e) => console.log(e.target.value);
+btnColor.onclick = () => sketchMode('color');
+btnRandom.onclick = () => sketchMode('rainbow')
+btnEraser.onclick = () => sketchMode('erase')
 // slider.addEventListener("change", (e) => ) 
 
 
@@ -20,12 +28,30 @@ function createTile () {
         tiles.style.width = `${GRID_DIM / dimension}px`;
         tiles.style.height = `${GRID_DIM / dimension}px`;
 
+        tiles.addEventListener('mouseover', paint)
+        tiles.addEventListener('mousedown', paint)
         
         sketchContainer.appendChild(tiles);
     }
 }  
 
-function paint (e) {
+function sketchMode (sketchColor) {
+    if (sketchColor === 'color') {
+        btnColor.classList.add('on');
+        btnRandom.classList.remove('on');
+        btnEraser.classList.remove('on');
+    } else if (sketchColor === 'rainbow') {
+        btnRandom.classList.add('on');
+        btnColor.classList.remove('on');
+        btnEraser.classList.remove('on');
+    } else if (sketchColor === 'eraser') {
+        btnEraser.classList.add('on');
+        btnColor.classList.remove('on');
+        btnRandom.classList.remove('on');
+    }
+}
+
+function paint(e) {
 
 }
 
